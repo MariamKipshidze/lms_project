a1c1f42fcd347b70c2e2631451341a9dd01bcdbc
mariami <mari.kifshidze@gmail.com>
2021-02-27 17:55:41 +0400

    adding detail view and using @api_view

--- student_app/urls.py
+++ student_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from student_app import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('student/profile/', views.student_profile_list),
+    path('student/profile/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- student_app/views.py
+++ student_app/views.py
@@ -1,21 +1,46 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
+
 from .models import StudentProfile
 from .serializers import StudentProfileSerializer
 
 
-@csrf_exempt
-def student_profile_list(request):
+@api_view(['GET', 'POST'])
+def student_profile_list(request, format=None):
     if request.method == 'GET':
         student_profile = StudentProfile.objects.all()
         serializer = StudentProfileSerializer(student_profile, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = StudentProfileSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None ):
+    try:
+        profile = StudentProfile.objects.get(pk=pk)
+    except StudentProfile.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentProfileSerializer(profile)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = StudentProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
