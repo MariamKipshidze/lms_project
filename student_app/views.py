d107ba90c2fb0ab5349ee4f551c9e0dfe8321bf0
mariami <mari.kifshidze@gmail.com>
2021-02-27 18:56:01 +0400

    generic class-based views

--- student_app/views.py
+++ student_app/views.py
@@ -1,31 +1,14 @@
 from .models import StudentProfile
 from .serializers import StudentProfileSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-
+class StudentList(generics.ListCreateAPIView):
     queryset = StudentProfile.objects.all()
     serializer_class = StudentProfileSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class StudentDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = StudentProfile.objects.all()
     serializer_class = StudentProfileSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
     
