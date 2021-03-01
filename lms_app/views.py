e5ee1e633e0ba36b58d8d67964067e12124ee1de
mariami <mari.kifshidze@gmail.com>
2021-03-01 19:01:07 +0400

    adding permissions to function view

--- lms_app/views.py
+++ lms_app/views.py
@@ -8,11 +8,13 @@ from rest_framework import generics
 from rest_framework import permissions
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
 def subject_detail(request, pk, format=None):
     try:
         subject = Subject.objects.get(pk=pk)
