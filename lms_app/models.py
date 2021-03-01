1aeebe4ad412e85d94d69a01ed227624fe34ef65
mariami <mari.kifshidze@gmail.com>
2021-03-01 18:33:50 +0400

    adding some fields to serializer

--- lms_app/models.py
+++ lms_app/models.py
@@ -23,15 +23,15 @@ class Subject(models.Model):
 
 
 class StudentProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("Student"))
-    faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT, verbose_name=_("Faculty"), default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("Student"), related_name="student_profile")
+    faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT, verbose_name=_("Faculty"), related_name="student")
     first_name = models.CharField(max_length=50, verbose_name=_("First Name"))
     last_name = models.CharField(max_length=50, verbose_name=_("Last Name"))
     mobile_number = models.CharField(max_length=20, verbose_name=_("Mobile Number"), default="")
     personal_id = models.CharField(max_length=11, verbose_name=_("Personal ID"), default="")
     image = models.ImageField(default="default.jpg", upload_to="profile_pics", verbose_name=_("Profile Picture"))
     gpa = models.DecimalField(max_digits=3, decimal_places=2, verbose_name=_('GPA'))
-    subject = models.ManyToManyField(Subject, verbose_name=_("Subject"))
+    subject = models.ManyToManyField(Subject, verbose_name=_("Subject"), related_name="student_subject")
 
     def __str__(self):
         return f"{self.user.email} {self.user.last_name} Profile"
@@ -47,7 +47,7 @@ class StudentProfile(models.Model):
 
 
 class LecturerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("Lecturer"))
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("Lecturer"), related_name="lecturer_profile")
     faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT, verbose_name=_("Faculty"))
     first_name = models.CharField(max_length=50, verbose_name=_("First Name"))
     last_name = models.CharField(max_length=50, verbose_name=_("Last Name"))
--- lms_app/serializers.py
+++ lms_app/serializers.py
@@ -6,26 +6,26 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "status"]
+        fields = ["email", "status", "student_profile", "lecturer_profile"]
 
-
-class SubjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subject
-        fields = ["name", "faculty", "credit_score", "lecturer"]
-
-
-class FacultySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Faculty
-        fields = ["name"]
-
-
+    
 class StudentProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentProfile
         fields = ["user", "faculty", "first_name", "last_name", "gpa", 
         "image", "mobile_number", "subject", "personal_id"]
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ["name", "faculty", "credit_score", "lecturer", "student_subject"]
+
+
+class FacultySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Faculty
+        fields = ["name", "student"]
 
 
 class LecturerProfileSerializer(serializers.ModelSerializer):
