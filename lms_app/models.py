8c3add790d13f7a311a396d73187928b192ca340
mariami <mari.kifshidze@gmail.com>
2021-03-03 18:48:16 +0400

    adding new fields, verbose_name and verbose_name_plural

--- lms_app/models.py
+++ lms_app/models.py
@@ -7,9 +7,15 @@ from PIL import Image
 
 class Faculty(models.Model):
     name = models.CharField(max_length=50, verbose_name=_("Faculty name"))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Price'))
+    description = models.TextField(verbose_name=_("Description"))
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('Faculty')
+        verbose_name_plural = _('Faculties')
 
 
 class Subject(models.Model):
@@ -21,9 +27,15 @@ class Subject(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = _('Subject')
+        verbose_name_plural = _('Subjects')
+
 
 class StudentProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("Student"), related_name="student_profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                verbose_name=_("Student"),
+                                related_name="student_profile")
     faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT, verbose_name=_("Faculty"), related_name="student")
     first_name = models.CharField(max_length=50, verbose_name=_("First Name"))
     last_name = models.CharField(max_length=50, verbose_name=_("Last Name"))
@@ -41,19 +53,29 @@ class StudentProfile(models.Model):
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size = (300,300)
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
+    class Meta:
+        verbose_name = _('Student')
+        verbose_name_plural = _('Students')
+
 
 class LecturerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("Lecturer"), related_name="lecturer_profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                verbose_name=_("Lecturer"),
+                                related_name="lecturer_profile")
     faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT, verbose_name=_("Faculty"))
     first_name = models.CharField(max_length=50, verbose_name=_("First Name"))
     last_name = models.CharField(max_length=50, verbose_name=_("Last Name"))
     mobile_number = models.CharField(max_length=20, verbose_name=_("Mobile Number"))
     personal_id = models.CharField(max_length=11, verbose_name=_("Personal ID"))
+    salary = models.DecimalField(max_digits=8, decimal_places=2, verbose_name=_('Salary'))
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} Profile"
 
+    class Meta:
+        verbose_name = _('Lecturer')
+        verbose_name_plural = _('Lecturers')
