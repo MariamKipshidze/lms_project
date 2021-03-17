cd91119765f2ae6f08b5ec6977e646f9a4aba721
mariami <mari.kifshidze@gmail.com>
2021-03-17 17:43:35 +0400

    setting configurations for postgre

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -102,9 +102,17 @@ WSGI_APPLICATION = 'lms_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbtest',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
